67a03fa2870fad69997a85190e2624c354d3ccd8
Your Name <you@example.com>
2025-11-04 22:47:35 +0700

    fix AR

--- components/product-details.tsx
+++ components/product-details.tsx
@@ -464,7 +464,7 @@ interface ImageZoomModalProps {
 function ImageZoomModal({ isOpen, onClose, imageUrl, productId }: ImageZoomModalProps) {
 
   // ID 2 = Nạp Cảm Hứng, ID 4 = Hồi ức đáng nhớ
-  const isLightBg = (productId === 2 || productId === 4 || productId === 3);
+  const isLightBg = ( productId === 4 || productId === 3);
   
   const overlayClass = "bg-black/80 backdrop-blur-sm";
     
